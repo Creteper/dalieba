02b282a7122602e204ffa475febc07e6eeb91629
JianLin416 <lijianlin050416@gmail.com>
2025-03-18 13:25:44 +0800

    添加字体

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,17 @@ import { ModeToggle } from "@/components/theme/ThemeToggle";
 
 export default function Main() {
 
-  function alt () {
-    alert("Hello world")
-  }
-  return (
-    <div className="flex items-center justify-center">
-      <Button size="icon" onClick={alt}>
-        <Home />
-      </Button>
-      <ModeToggle />
-      
-    </div>
-  );
+    function alt() {
+        alert("Hello world")
+    }
+    return (
+        <div className="flex items-center justify-center">
+            <Button size="icon" onClick={alt}>
+                <Home />
+            </Button>
+            <ModeToggle />
+            <p className="text-9xl font-bold">字体预览</p>
+            <p className="text-9xl font-bold">Typeface</p>
+        </div>
+    );
 }
