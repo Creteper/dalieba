ed1a433538f978a71e0ecb8ee29a49ef6d8030ba
Creteper <7512254@qq.com>
2025-03-22 13:20:48 +0800

    rele

--- app/home/computer/page.tsx
+++ app/home/computer/page.tsx
@@ -2,21 +2,15 @@
  * @Author: Creteper 7512254@qq.com
  * @Date: 2025-03-22 13:16:50
  * @LastEditors: Creteper 7512254@qq.com
- * @LastEditTime: 2025-03-22 13:19:31
+ * @LastEditTime: 2025-03-22 13:17:25
  * @FilePath: \dalieba\app\home\page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 'use client'
 
-import { useIsMobile } from "@/hooks/use-mobile"
-import { useEffect } from "react"
 
-export default function HomePage () {
-  const isMobile = useIsMobile()
-  useEffect(() => {
-    console.log(isMobile)
-  })
+export default function HomePCPage () {
   return (
     <div>
       HELLO WORLD
--- app/home/mobile/page.tsx
+++ app/home/mobile/page.tsx
@@ -2,21 +2,15 @@
  * @Author: Creteper 7512254@qq.com
  * @Date: 2025-03-22 13:16:50
  * @LastEditors: Creteper 7512254@qq.com
- * @LastEditTime: 2025-03-22 13:19:31
+ * @LastEditTime: 2025-03-22 13:18:33
  * @FilePath: \dalieba\app\home\page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 'use client'
 
-import { useIsMobile } from "@/hooks/use-mobile"
-import { useEffect } from "react"
 
-export default function HomePage () {
-  const isMobile = useIsMobile()
-  useEffect(() => {
-    console.log(isMobile)
-  })
+export default function HomeMPage () {
   return (
     <div>
       HELLO WORLD
